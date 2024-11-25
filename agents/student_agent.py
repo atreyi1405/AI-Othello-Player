e842e26b70b0047d65389bd8119a19ef4d94c0a4
atreyi <ranjanatreyi@gmail.com>
2024-11-25 18:16:58 -0500

    a little over 2 seconds max turn time, evaluating board state, much stronger heuristic and better performance

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -4,48 +4,42 @@ import numpy as np
 from copy import deepcopy
 import time
 import math
-from helpers import random_move, count_capture, execute_move, check_endgame, get_valid_moves
+from helpers import random_move, count_capture, execute_move, check_endgame, get_valid_moves, get_directions
 
+#ignore
 class Node:
     def __init__(self, chess_board, player=None, move=None, parent=None):
-        self.chess_board=chess_board
-        self.player=player
-        self.parent=parent
-        self.move=move
-        self.children=[]
-        self.visit=0
-        self.wins=0
-    
-    def uct(self,c=1.0):
-        if self.visit==0:
+        self.chess_board = chess_board
+        self.player = player
+        self.parent = parent
+        self.move = move
+        self.children = []
+        self.visit = 0
+        self.wins = 0
+
+    def uct(self, c=1.0):
+        if self.visit == 0:
             return float('inf')
-        exploitation=self.wins/self.visit
-        exploration= c * np.sqrt((np.log(self.parent.visits))/self.visit)
+        exploitation = self.wins / self.visit
+        exploration = c * math.sqrt(math.log(self.parent.visit) / self.visit)
         return exploitation + exploration
-  
 
 @register_agent("student_agent")
 class StudentAgent(Agent):
     def __init__(self):
         super(StudentAgent, self).__init__()
         self.name = "StudentAgent"
-    
+
     def step(self, chess_board, player, opponent):
-        start=time.time()
-        self.max_depth = 3  
-        self.corners = [(0, 0), (0, -1), (-1, 0), (-1, -1)]
-        self.cornerVal = 100
-        for depth in range(1, self.max_depth+1): 
-            if time.time() - start >= 1.9:
-                break
-            _, move = self.alphabeta(chess_board, player, opponent, depth, isMax=True)
-            best_move = move
-        #return best_move
-        #_, best_move = self.alphabeta(chess_board, player, opponent, depth=self.max_depth, isMax=True)
+        start_time = time.time()
+        depth=3 #the tradeoff to being strict with time is digging less deeper, makes it easier for a human to win 
+        #self.max_depth = 3, changed this to dynamically adjust depth (suggestion to improve time )
+        best_move = None
+        _, move= self.alphabeta(chess_board,player,opponent,depth, isMax=True,start_time=start_time)
+        if move is not None:
+            best_move=move
         return best_move
         #return self.greedyFlips(chess_board, player, opponent)
-        #todo: make the heuristic stronger
-        #todo: try monte-carlo, upper confidence ?
         
 
     def greedyFlips(self, chess_board, player, opponent):
@@ -116,65 +110,110 @@ class StudentAgent(Agent):
             #print("found the best move!! ", best_move)
             time_taken = time.time() - start_time
             #print("My minimax AI's turn took ", time_taken, "seconds.")
-            return value, best_move 
+            return value, best_move
+    
+    
+    def eval(self, chess_board, player, opponent):
+        corners = [(0, 0), (0, len(chess_board)-1), (len(chess_board)-1, 0), (len(chess_board)-1, len(chess_board)-1)]
+        cornerVal = 100
+        #game stage for better strategies throughout --https://medium.com/@gmu1233/how-to-write-an-othello-ai-with-alpha-beta-search-58131ffe67eb, chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/https://barberalec.github.io/pdf/An_Analysis_of_Othello_AI_Strategies.pdf
+        total_squares = len(chess_board)*len(chess_board)
+        total_pieces = np.sum(chess_board != 0) #occupied
+        stage = total_pieces / total_squares 
+        corner_score=0 
+        stability_score=0
+        mobility_score=0 
+        piece_score=0 
 
-      
-    def eval(self, chess_board, player, opponent, score):
-        for corner in self.corners:
-            if chess_board[corner[0]][corner[1]]== player:
-                #print("player found a corner")
-                score += self.cornerVal
+        #corners are always better
+        for corner in corners:
+            if chess_board[corner[0]][corner[1]] == player:
+                corner_score += cornerVal
             elif chess_board[corner[0]][corner[1]] == opponent:
-                    #print("opponent found a corner")
-                    score -= self.cornerVal
-        return score
+                corner_score -= cornerVal
 
-    def alphabeta(self, chess_board, player, opponent, depth, isMax, alpha=float('-inf'), beta=float('inf')):
-        print("We're at depth: ", depth, "...")
-        best_move=None
-        is_endgame, player_score, opponent_score = check_endgame(chess_board, player, opponent)
-        #call eval here, prioritising corners?
-        score = self.eval(chess_board, player, opponent, player_score-opponent_score)
+        #stability measures how vulnerable the disc is to be flanked
+        for row in range(len(chess_board)):
+            for col in range(len(chess_board[row])):
+                if chess_board[row][col] == player and self.is_stable(chess_board, row, col, player):
+                    stability_score += 10
+                elif chess_board[row][col] == opponent and self.is_stable(chess_board, row, col, opponent):
+                    stability_score -= 10
+
+        #mobility measures how many moves the player has
+        player_moves = len(get_valid_moves(chess_board, player))
+        opponent_moves = len(get_valid_moves(chess_board, opponent))
+        mobility_score = 10 * (player_moves - opponent_moves)
+
+        #pieve count- i believe this is not as important--https://www.ultraboardgames.com/othello/strategy.php
+        player_pieces = np.sum(chess_board == player)
+        opponent_pieces = np.sum(chess_board == opponent)
+        piece_score = player_pieces - opponent_pieces
+
+        #it's better to prioritise different things in different stages
+        if stage < 0.3:
+            #print("total pieces occupied so far: ", total_pieces, "so this is the early stage, corners are imp")
+            return 0.5 * mobility_score + 1.5 * corner_score + 0.5 * stability_score #i actually am not sure about these numbers loll, trial and error-- have to make the early stage more attacking tho
+        elif stage < 0.7:
+            #print("total pieces occupied so far: ", total_pieces, "so this is the mid stage")
+            return 1.0 * mobility_score + 1.5 * corner_score + 0.8 * stability_score + 0.2 * piece_score #gets slightly better at attcking
+        else:
+            #print("total pieces occupied so far: ", total_pieces, "so this is the late stage, corners are imp")
+            return 0.2 * mobility_score + 2.0 * corner_score + 1.0 * piece_score + 0.5 * stability_score #it does better against random in the late stages, but might lose to human agent
+
+    def is_stable(self, chess_board, row, col, player):
+        if (row == 0 or row == len(chess_board) - 1) or (col == 0 or col == len(chess_board[0]) - 1):
+            return True
+        directions=get_directions()
+        for dr, dc in directions:
+            r, c = row + dr, col + dc
+            if 0 <= r < len(chess_board) and 0 <= c < len(chess_board[0]) and chess_board[r][c] != player:
+                return False
+        return True
+
+    def order_moves(self, chess_board, moves, player): 
+        return sorted(moves, key=lambda move: self.eval(self.exec(chess_board, move, player), player, 3 - player), reverse=True)
+    def exec(self, chess_board, move, player):
+        new_board = deepcopy(chess_board)
+        execute_move(new_board, move, player)
+        return new_board
+
+    def alphabeta(self, chess_board, player, opponent, depth, isMax, alpha=float('-inf'), beta=float('inf'), start_time=None):
+        #print("We're at depth: ", depth, "...")
+        if time.time() - start_time >= 1.9:
+            return 0, None  #the max turn time was going up till 5 seconds
+        best_move = None
+        is_endgame, _, _ = check_endgame(chess_board, player, opponent)
         if depth == 0 or is_endgame:
-            #score = player_score - opponent_score
-            return score, best_move #then just return score, best_move?
-        if isMax: #maximising player
-            start_time = time.time()
+            return self.eval(chess_board, player, opponent), None
+        legal_moves = self.order_moves(chess_board, get_valid_moves(chess_board, player if isMax else opponent), player if isMax else opponent)
+        #print(legal_moves)
+        if isMax:  #maximising player
             value = float('-inf')
-            legal_player_moves = get_valid_moves(chess_board, player)
-            #print(legal_player_moves)
-            for move in legal_player_moves:
-                new_board = deepcopy(chess_board)
-                execute_move(new_board, move, player)
-                move_value = self.alphabeta(new_board, player, opponent, depth - 1, False, alpha,beta)
-                #print("okay move value you get in opponent is this:  ", move_value)
-                if move_value[0] > value:
-                    value = move_value[0]
+            for move in legal_moves:
+                new_board = self.exec(chess_board, move, player)
+                move_value, _ = self.alphabeta(new_board, player, opponent, depth - 1, False, alpha, beta, start_time)
+                if move_value > value:
+                    value = move_value
                     best_move = move
                 alpha = max(alpha, value)
-                if alpha >= beta: #this is the difference, immediately break: now we're avoiding the repeating branches, significantly reducing time!
-                    break
-            #print("found the best move!! ", best_move)
-            time_taken = time.time() - start_time
-            print("My alphabeta AI's turn took ", time_taken, "seconds.")
-            return value,best_move
-        else:
-            start_time = time.time()
-            value = float('inf')
-            legal_opponent_moves = get_valid_moves(chess_board, opponent)
-            for move in legal_opponent_moves:
-                new_board = deepcopy(chess_board)
-                execute_move(new_board, move, opponent)
-                move_value = self.alphabeta(new_board, player, opponent, depth - 1, True, alpha,beta)
-                #print("okay move value you get in opponent is this:  ", move_value)
-                if move_value[0] < value:
-                    value = move_value[0]
-                    best_move = move
-                beta = min(alpha, value)
                 if alpha >= beta:
                     break
-            #print("found the best move!! ", best_move)
-            time_taken = time.time() - start_time
+            time_taken=time.time()-start_time
             print("My alphabeta AI's turn took ", time_taken, "seconds.")
-            return value,best_move
+            return value, best_move
+        else:  #minimising player
+            value = float('inf')
+            for move in legal_moves:
+                new_board = self.exec(chess_board, move, opponent)
+                move_value, _ = self.alphabeta(new_board, player, opponent, depth - 1, True, alpha, beta, start_time)
+                if move_value < value:
+                    value = move_value
+                    best_move = move
+                beta = min(beta, value)
+                if alpha >= beta:
+                    break
+            time_taken=time.time()-start_time
+            print("My alphabeta AI's turn took ", time_taken, "seconds.")
+            return value, best_move
 
